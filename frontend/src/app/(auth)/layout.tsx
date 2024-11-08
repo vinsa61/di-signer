94c4c40282695ef085ad3121a609d4fca3f1e438
vinsa61 <vinsarioshen610@gmail.com>
2024-11-08 16:53:09 +0700

    login signup pages

---
+++ frontend/src/app/(auth)/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "../globals.css";
+
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+const spaceMono = localFont({
+  src: "../fonts/SpaceMono-Regular.woff",
+  variable: "--space-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Diner",
+  description: "Digital Signature Protocol",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head></head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${spaceMono.variable} antialiased bg-black dark`}
+      >
+        <div className="flex flex-col items-center justify-center min-h-screen">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
